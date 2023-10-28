897cbd77a677736985ce6411b7836b752aaf402b
Colin Daniel <colin.daniel1227@gmail.com>
2023-10-28 18:59:46 -0400

    allOfChar

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const g = require('./swe_wordlist.json')
+const { wordList } = require('./swe_wordlist.json')
 
-console.log(g.wordList[10000])
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'å', 'ö']
+
+
+const allOfChar = (char) => {
+    const r = wordList.filter((word) => {
+       return word[0] === char;
+    });
+    return r;
+};
+
+const t = allOfChar('z')
