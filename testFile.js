0183aa567f6d22f14b42ae8c3953466d2ca9f5c3
Colin Daniel <colin.daniel1227@gmail.com>
2023-10-30 23:01:13 -0400

    testFile created, 410,785 words in EN and SWE

---
+++ testFile.js
@@ -0,0 +1,7 @@
+const en = require('./en_wordlist_final.json');
+const swe = require('./swe_wordlist.json');
+
+console.log(`
+English wordlist length: ${en.wordList.length}
+Swedish wordlist length: ${swe.wordList.length}
+`)
